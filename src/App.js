9bb7e4f1929dace4dd704958717df8fbaa8c6ec3
Composta Puerta <adkadkwkq12@gmail.com>
2023-12-13 21:54:57 -0500

    upload

--- src/App.js
+++ src/App.js
@@ -59,13 +59,13 @@ const initialState = {
     }
 }
 
-class App extends Component{
-    constructor(){
+class App extends Component {
+    constructor() {
         super();
-        this.state =  initialState;
+        this.state = initialState;
     }
 
-    loadUser = (data) =>{
+    loadUser = (data) => {
         this.setState({
             user:{
                 id: data.id,
@@ -77,92 +77,10 @@ class App extends Component{
         })
     }
 
-    calculateFaceLocation = (data) =>{
-        console.log(data);
-        const clarifaiFace =  data.outputs[0].data.regions[0].region_info.bounding_box;
-        const image = document.getElementById('inputImage');
-        const width = Number(image.width);
-        const height = Number(image.height);
 
-        return{
-            leftCol: clarifaiFace.left_col * width,
-            topRow: clarifaiFace.top_row * height,
-            rightCol: width - (clarifaiFace.right_col * width),
-            bottomRow: height - (clarifaiFace.bottom_row * height)
-        }
-    }
-
-    calculateFacesLocations = (data) => {
-        const regions = data.outputs[0].data.regions;
-        const image = document.getElementById('inputImage');
-        const width = Number(image.width);
-        const height = Number(image.height);
-        let faces = [];
-        regions.forEach(element => {
-            const clarifaiFace = element.region_info.bounding_box;
-            const box = {
-                leftCol: clarifaiFace.left_col * width,
-                topRow: clarifaiFace.top_row * height,
-                rightCol: width - (clarifaiFace.right_col * width),
-                bottomRow: height - (clarifaiFace.bottom_row * height)
-            }
-            faces.push(box);
-        });
-        this.displayBox(faces);
-    }
-
-    displayBox = (box) =>{
-        console.log(box);
-        this.setState({box : box});
-    }
-
-    onInputChange = (event) =>{
-        this.setState({input: event.target.value});
-    }
-
-    onButtonSubmit = () =>{
-        this.setState({imageUrl: this.state.input});
-        // app.models
-        // .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-        fetch('https://obscure-oasis-20536.herokuapp.com/imageUrl',{
-            method: 'post',
-            headers: {'Content-Type' : 'application/json'},
-            body: JSON.stringify({
-                input: this.state.input,
-            })
-        })
-        .then(response => response.json())
-        .then(response =>{
-            if(response){
-                fetch('https://obscure-oasis-20536.herokuapp.com/image',{
-                    method: 'put',
-                    headers: {'Content-Type' : 'application/json'},
-                    body: JSON.stringify({
-                        id: this.state.user.id,
-                    })
-                })
-                .then(response => response.json())
-                .then(count =>{
-                    this.setState(Object.assign(this.state.user, {entries: count}))
-                })
-                .catch(console.log);
-            }
-            this.calculateFacesLocations(response);
-        })
-        .catch(err => console.log(err));
-    }
-
-    onRouteChange = (route) =>{
-        if(route === 'signIn' || route === 'register')
-            this.setState(initialState);
-        else if(route === 'home')
-            this.setState({isSignedIn: true});
-        this.setState({route: route});
-    }
-
-    render(){
+    render() {
         const { imageUrl, box, route, isSignedIn } = this.state;
-        return(
+        return (
             <div className="App">
                 <Particles className='particles' params={particlesOptions} />
                 <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
